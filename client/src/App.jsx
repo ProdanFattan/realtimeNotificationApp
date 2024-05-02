6336865c69924bf341963def105e59866968e59d
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-05-02 09:37:06 +0600

    simple login page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
-
-
+import { useState } from "react";
+import "./App.css";
 function App() {
-
-
+  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
+  console.log(user);
   return (
     <>
-     <p>Hello World</p>
+      <div className="container">
+        <div className="login">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(username)}>Login</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
